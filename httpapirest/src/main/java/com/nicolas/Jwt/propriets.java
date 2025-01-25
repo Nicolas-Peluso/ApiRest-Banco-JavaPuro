cf55d32ae74078505c15276cee1b88bd3f177609
Nicolas-Peluso <pelusonicolas7@gmail.com>
2025-01-25 18:30:35 -0300

    arquivo propriets deve ser publico para caso alguem queira testar

---
+++ httpapirest/src/main/java/com/nicolas/Jwt/propriets.java
@@ -0,0 +1,9 @@
+package com.nicolas.Jwt;
+
+public class propriets {
+    private String JWT_SECRET_WORD = "J97uj#12!8Hg&kT09vXm$qRtP@LbEw57";
+
+    public String getJWT_SECRET_WORD() {
+        return JWT_SECRET_WORD;
+    }
+}
